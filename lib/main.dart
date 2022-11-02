a32f0403ea6a98acb72b9b2b142eb78c51ddfc06
leonmsdev <leonms.dev@gmail.com>
2022-11-02 19:31:47 +0100

    feat: add navigation between login and register screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_dart/firebase_options.dart';
-import 'package:learn_dart/screens/main_screen.dart';
+import 'package:learn_dart/screens/login_screen.dart';
 import 'package:learn_dart/screens/register_screen.dart';
 
 void main() {
@@ -17,6 +17,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
+      routes: {
+        '/login/': (context) => const LoginScreen(title: 'login Screen'),
+        '/register/': (context) => const RegisterScreen(),
+      },
     ),
   );
 }
@@ -26,27 +30,26 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Center(child: Text('loading'));
-            case ConnectionState.done:
-              final FirebaseAuth auth = FirebaseAuth.instance;
-              final User? user = auth.currentUser;
-              if (user?.emailVerified ?? false) {
-                return const MainScreen();
-              } else {
-                return const RegisterScreen(title: 'Register Screen');
-              }
-            default:
-              return const Center(child: Text('something went wrong'));
-          }
-        },
-      ),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final FirebaseAuth auth = FirebaseAuth.instance;
+            final User? user = auth.currentUser;
+
+            return const LoginScreen(title: 'Login');
+          //   if (user?.emailVerified ?? false) {
+          //     return const Text('Done');
+          //   } else {
+          //     return VerifyEmailScreen(auth: auth);
+          //   }
+
+          default:
+            return const Center(child: Text('something went wrong'));
+        }
+      },
     );
   }
 }
