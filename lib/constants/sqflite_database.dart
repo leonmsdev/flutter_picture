b77f33c75a0aebfacc55829162c1072a2f6a6eb5
leonmsdev <leonms.dev@gmail.com>
2022-11-12 19:00:11 +0100

    feat: implement notes_db CRUD logic for notes and users

---
+++ lib/constants/sqflite_database.dart
@@ -0,0 +1,24 @@
+//Const that define sqflite columns
+const dbName = 'notes.db';
+const noteTable = 'note';
+const userTable = 'user';
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedColumn = 'is_synced';
+const createUserTable = ''' 
+      CREATE TABLE IF NOT EXISTS "user"  (
+	    "id"	INTEGER NOT NULL,
+	    "email"	TEXT NOT NULL UNIQUE,
+	    PRIMARY KEY("id" AUTOINCREMENT)
+      );''';
+const createNoteTable = ''' 
+      CREATE TABLE IF NOT EXISTS "note" (
+	    "id"	INTEGER NOT NULL,
+	    "user_id"	INTEGER NOT NULL,
+	    "text"	TEXT NOT NULL,
+	    "is_synced"	INTEGER NOT NULL DEFAULT 0,
+	    FOREIGN KEY("user_id") REFERENCES "user"("id"),
+	    PRIMARY KEY("id" AUTOINCREMENT)
+      );''';
